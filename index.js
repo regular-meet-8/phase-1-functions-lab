bec5647d6023cfca51821d4ae684f869540c5b7b
regular-meet-8 <eddiekim444@gmail.com>
2022-11-25 11:32:46 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blockNum){
+    if ( blockNum < 42 ){
+        console.log (42 - blockNum) 
+    }else if( blockNum > 42 ){
+         console.log (blockNum - 42 )
+
+    }
+    
+}
+function distanceFromHqInFeet(){
+function distanceFromHqInBlocks(blockNum) ;
+
+ console.log (distanceFromHqInBlocks(blockNum) * 264) 
+
+}
+function distanceTravelledInFeet(start, destination){
+    if(start > destination){
+         console.log((start - destination) * 264) 
+
+    }else if(destination > start){
+        console.log ((destination-start) * 264 )
+  }
+
+}
+ function calculatesFarePrice(start, destination) {
+    function distanceFromHqInFeet(start, destination);
+    if(distanceFromHqInFeet(start, destination)< 400){
+        console.log (`You have a free sample.`)
+    }if(distanceFromHqInFeet(start, destination)>400 ){
+        price = distanceFromHqInFeet(start, destination) * 2
+        console.log( price) 
+    }if (distanceFromHqInFeet(start, destination)> 2000){
+        price =distanceFromHqInFeet(start, destination) * 25
+console.log( price) 
+
+    }if (distanceFromHqInFeet(start, destination)> 2500){
+        console.log (`cannot travel that far`)
+    }
+
+}
+calculatesFarePrice( 45, 42)
